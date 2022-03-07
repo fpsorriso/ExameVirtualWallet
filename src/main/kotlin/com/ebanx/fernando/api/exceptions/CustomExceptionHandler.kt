b2a9308836509ba0baf1663a17caece603213736
Fernando Paulino da Silva <fpsorriso@gmail.com>
2022-03-07 10:06:04 -0300

    Creates exceptions handlers (#2)

---
+++ src/main/kotlin/com/ebanx/fernando/api/exceptions/CustomExceptionHandler.kt
@@ -0,0 +1,21 @@
+package com.ebanx.fernando.api.exceptions
+
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@ControllerAdvice
+class CustomExceptionHandler : ResponseEntityExceptionHandler() {
+    private val logger = LoggerFactory.getLogger(CustomExceptionHandler::class.java)
+
+    @ExceptionHandler(value = [AccountNotFoundException::class, BalancesNotFoundException::class])
+    fun notFoundHandler(ex: Exception,  request: WebRequest): ResponseEntity<Int> {
+        logger.warn("Not found values", ex)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(0)
+    }
+
+}
