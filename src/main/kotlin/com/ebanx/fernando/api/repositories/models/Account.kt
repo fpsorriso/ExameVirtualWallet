9712bdb9a4ecb4f6a5a127cea818163c9a29e2bc
Fernando Paulino da Silva <fpsorriso@gmail.com>
2022-03-07 10:28:40 -0300

    Creates account model and repository with implementation (#6)

---
+++ src/main/kotlin/com/ebanx/fernando/api/repositories/models/Account.kt
@@ -0,0 +1,19 @@
+package com.ebanx.fernando.api.repositories.models
+
+data class Account(val id: String, var amount: Int) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Account
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
